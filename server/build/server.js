45eea25097732e033154f3da2f5b9b8e58ee4e06
valdir-alves3000 <valdiralves3000@gmail.com>
2022-09-13 21:26:37 -0300

    Stage 1 | Base Building

---
+++ server/build/server.js
@@ -0,0 +1,4 @@
+import express from "express";
+const app = express();
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server in running port ${PORT}`));
---
+++ web/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hello NLW eSPORTS</h1>;
+}
+
+export default App;
